f69e399e838a0a7382b5a570d828f053088c0b6c
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-04 15:52:20 -0500

    aboutme favs

--- server.rb
+++ server.rb
@@ -8,10 +8,18 @@ end
   erb (:index)
 end
 
-
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb (:about_me)
 end
+
+get '/favourites' do
+  @fav_links = ['terminator2', 'aliens', 'star wars', 'predator', 'rocky']
+    erb (:favourites)
+end
+
+
 
 get '/portfolio' do
   erb (:gallery)
