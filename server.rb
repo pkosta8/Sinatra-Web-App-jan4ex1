470022f86fafd2aff91bdc0e4be36c9dce7d286a
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-04 15:01:46 -0500

    aboutme

--- server.rb
+++ server.rb
@@ -1,10 +1,19 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
   get '/home' do
-  erb :index
+  erb (:index)
 end
 
 
+get '/about_me' do
+  erb (:about_me)
+end
+
 get '/portfolio' do
-  erb :gallery
+  erb (:gallery)
+end
 end
